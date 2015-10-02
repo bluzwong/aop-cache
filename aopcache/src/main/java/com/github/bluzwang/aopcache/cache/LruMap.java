d4d9311564ac730d5a7542461199236d98d846ca
bruce.home <766796371@qq.com>
2015-10-02 17:03:35 +0800

    release debuggable true

--- aopcache/src/main/java/com/github/bluzwang/aopcache/cache/LruMap.java
+++ aopcache/src/main/java/com/github/bluzwang/aopcache/cache/LruMap.java
@@ -6,11 +6,8 @@ import java.util.LinkedHashMap;
  * Created by wangzhijie@wind-mobi.com on 2015/9/15.
  */
 public class LruMap<K,V> extends LinkedHashMap<K,V> {
-    /** serialVersionUID */
     private static final long serialVersionUID = -5933045562735378538L;
-    /** 最大数据存储容量 */
     private static final int  LRU_MAX_CAPACITY     = 1024;
-    /** 存储数据容量  */
     private int               capacity = LRU_MAX_CAPACITY;
 
     public LruMap() {
@@ -18,10 +15,6 @@ public class LruMap<K,V> extends LinkedHashMap<K,V> {
     }
 
     /**
-     * 带参数构造方法
-     * @param initialCapacity   容量
-     * @param loadFactor        装载因子
-     * @param isLRU             是否使用lru算法，true：使用（按方案顺序排序）;false：不使用（按存储顺序排序）
      */
     public LruMap(int initialCapacity, float loadFactor, boolean isLRU) {
         super(initialCapacity, loadFactor, true);
